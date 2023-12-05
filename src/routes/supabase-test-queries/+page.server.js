a8e0c29eaa2f517300cd7906f66030a825b96502
Siem.p <64197688+Siem-P@users.noreply.github.com>
2023-12-05 14:24:00 +0100

    Added Supabase package

---
+++ src/routes/supabase-test-queries/+page.server.js
@@ -0,0 +1,9 @@
+import { supabase } from "$lib";
+
+
+export async function load() {
+  const { data } = await supabase.from('teams').select('*');
+  console.log(data);
+  return { teams: data };
+}
+
---
+++ src/routes/supabase-test-queries/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let data
+
+    const { teams } = data
+</script>
+
+<section class="all-teams">
+    {#each teams as team}
+    <div>
+        <img src="/images/raimon.png" alt="Raimon Logo">
+        <p>{team.name}</p>
+    </div>
+    {/each}
+</section>
