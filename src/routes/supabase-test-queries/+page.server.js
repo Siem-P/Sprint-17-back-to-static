b981afe2d577f615b49a0b9ee0c4c39b0ac96dd6
Siem.p <64197688+Siem-P@users.noreply.github.com>
2023-12-05 14:56:04 +0100

    Export fix

--- src/routes/supabase-test-queries/+page.server.js
+++ src/routes/supabase-test-queries/+page.server.js
@@ -1,6 +1,5 @@
 import { supabase } from "$lib";
 
-
 export async function load() {
   const { data } = await supabase.from('teams').select('*');
   console.log(data);
--- src/routes/supabase-test-queries/+page.svelte
+++ src/routes/supabase-test-queries/+page.svelte
@@ -1,14 +1,15 @@
 <script>
     export let data
-
+    console.log(data)
     const { teams } = data
 </script>
 
-<section class="all-teams">
+<!-- <section class="all-teams">
     {#each teams as team}
     <div>
         <img src="/images/raimon.png" alt="Raimon Logo">
         <p>{team.name}</p>
     </div>
-    {/each}
-</section>
+    {/each} 
+</section> -->
+
